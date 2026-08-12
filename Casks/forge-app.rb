1d777732759d5e52a16840d3310b585239b908a8
Ryan Stoffel <stoffel.thomas.ryan@gmail.com>
2026-08-12 05:23:06 +0000

    forge: update edge to 5016396

--- Casks/forge-app.rb
+++ Casks/forge-app.rb
@@ -1,12 +1,12 @@
 cask "forge-app" do
-  version "0.1.0-edge.3c9eb4a"
+  version "0.1.0-edge.5016396"
 
   on_arm do
-    sha256 "2f697c1d4a04e7ccd6ef4838af588a88ba3144995b763d379a0ef4826b77e838"
+    sha256 "51f8624fb4e7e6be417e0ea053b64870a2f2e2397dce7f45cdc06fea77a458ef"
     url "https://github.com/RyanStoffel/forge/releases/download/forge-app-#{version}/Forge-aarch64-apple-darwin.zip"
   end
   on_intel do
-    sha256 "400fd92f0a000d9e4d8750edc83147b85151a05edde262f732c11c275399f389"
+    sha256 "d7b438863b45f9fe21731626adfc2c6485ce8f69d6fcfa6c1bbbbaa69a85fb74"
     url "https://github.com/RyanStoffel/forge/releases/download/forge-app-#{version}/Forge-x86_64-apple-darwin.zip"
   end
 
@@ -18,7 +18,7 @@ cask "forge-app" do
     skip "Forge app casks follow the project's continuously published edge channel"
   end
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "Forge.app"
   binary "#{appdir}/Forge.app/Contents/MacOS/forge"
