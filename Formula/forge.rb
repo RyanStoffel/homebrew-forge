08e0fb63a39a3b78ae898a671d691c55d8bb46de
Ryan Stoffel <stoffel.thomas.ryan@gmail.com>
2026-08-12 15:08:43 +0000

    forge: update edge to 6529712

--- Formula/forge.rb
+++ Formula/forge.rb
@@ -1,16 +1,16 @@
 class Forge < Formula
   desc "Native terminal, editor, Git, and coding-agent workspace"
   homepage "https://github.com/RyanStoffel/forge"
-  version "0.1.0-edge.5016396"
+  version "0.1.0-edge.6529712"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/RyanStoffel/forge/releases/download/edge/forge-aarch64-apple-darwin"
-      sha256 "e617c61df6063dc8a9fc841975bc8ecdfabbacd6108cf5c2c1b0bd86a2ce2c14"
+      sha256 "e1740ce929888be540aa2a1ab16a293035afef290daea3d1b3b72322312db67e"
     else
       url "https://github.com/RyanStoffel/forge/releases/download/edge/forge-x86_64-apple-darwin"
-      sha256 "94bde3c9076ae390421dc69d5b1716b82ed49aee0ea44081ef33008772418148"
+      sha256 "e2c41c836cdf72f3440372f7ca3c49afcc397081ee3cf52ed0bc668cb91a72cc"
     end
   end
 
