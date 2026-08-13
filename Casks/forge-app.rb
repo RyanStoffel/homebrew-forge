1ae33587055f4bd3801a428ba5088cabd19d14c2
Ryan Stoffel <stoffel.thomas.ryan@gmail.com>
2026-08-13 02:59:31 +0000

    forge: update edge to b8b5267

--- Casks/forge-app.rb
+++ Casks/forge-app.rb
@@ -1,12 +1,12 @@
 cask "forge-app" do
-  version "0.1.0-edge.e1b322f"
+  version "0.1.0-edge.b8b5267"
 
   on_arm do
-    sha256 "66a099b87d523b74d8c19f08a7eedb6ddf6e2da035267c28574dfa77c16b83df"
+    sha256 "e0d1d779cdd05b9c5ffca39877b7269263073cab8f5115615eabfc6daf3e3bf9"
     url "https://github.com/RyanStoffel/forge/releases/download/forge-app-#{version}/Forge-aarch64-apple-darwin.zip"
   end
   on_intel do
-    sha256 "7f38c3644398154529d1a34e5c38f01cd3bd068f024df142ea7bfc2861f2fcd1"
+    sha256 "bd5c44afa2c83c235adbbb96e0b7a76bfbcf9f1eada379768907f8e4ed83944d"
     url "https://github.com/RyanStoffel/forge/releases/download/forge-app-#{version}/Forge-x86_64-apple-darwin.zip"
   end
 
